6dce67fb2d971edf58178c9f745ca2c8463da1dd
IsaacReefman <80228892+IsaacReefman@users.noreply.github.com>
2025-10-16 00:47:04 +1100

    Update main.js

--- music/main.js
+++ music/main.js
@@ -2,8 +2,7 @@ fetch('/music/header.html')
   .then(res => res.text())
   .then(html => document.getElementById('header').innerHTML = html);
   
-fetch('
-  /music/footer.html')
+fetch('/music/footer.html')
   .then(res => res.text())
   .then(html => document.getElementById('site-footer').innerHTML = html);
 
