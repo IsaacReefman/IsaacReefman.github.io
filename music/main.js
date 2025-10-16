f2057ed0d294d253e3337df71ac19b694bd12722
IsaacReefman <80228892+IsaacReefman@users.noreply.github.com>
2025-10-16 16:21:40 +1100

    Update main.js

--- music/main.js
+++ music/main.js
@@ -5,3 +5,6 @@ fetch('/music/header.html')
 fetch('/music/footer.html')
   .then(res => res.text())
   .then(html => document.getElementById('site-footer').innerHTML = html);
+
+const y = String(new Date().getFullYear());
+ids.forEach(id => { const el = document.getElementById(id); if (el) el.textContent = y; });
